835f704e1269e9e33cb392debf77e54dbd386627
Amber <mallen@v0lture.com>
2017-03-18 10:50:44 -0600

    Added legal policies placeholder
    We missed a file to placehold, but it is here now. 👍

---
+++ policies.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- SEO -->
+    <title>v0lture Solutions: Policies</title>
+    <meta charset="utf-8">
+    <meta name="description" content="v0lture Solutions legal policies">
+
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"  rel="stylesheet">
+
+    <!-- resources -->
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <!-- scripts -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="js/main.js"></script>
+
+    <?php include "php/analytics.php"; ?>
+
+    <link rel="shortcut icon" type="image/x-icon" href="img/logo.png" />
+  </head>
+  <body>
+    <!-- status bar -->
+    <div class="statusbar">
+      <div class="left">
+        <a class="white-text">Policies</a>
+      </div>
+      <div class="right">
+        <a href="stats.php">v0lture Stats</a>
+        <a href="support.php">Support</a>
+        <a href="https://client.v0lture.com">Client Panel</a>
+      </div>
+    </div>
+
+    <!-- nav -->
+    <div class="navbar-fixed">
+      <nav>
+        <div class="nav-wrapper">
+          <a href="index.php" class="brand-logo">v0lture <sub class="hide-on-med-and-down"><small>Solutions</small></sub></a>
+          <a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+
+          <!-- mobile nav -->
+          <ul class="side-nav" id="mobile">
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="solutions.php">Solutions</a></li>
+          </ul>
+
+          <!-- normal nav -->
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li class="waves-effect waves-light"><a href="about.php">About</a></li>
+            <li class="waves-effect waves-light"><a href="contact.php">Contact</a></li>
+            <li class="waves-effect waves-light"><a href="solutions.php">Solutions</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <br />
+    <div align="center" class="white-text">
+      <img src="img/logo.png" alt="v0lture Solutions Logo" style="width:10%;height:10%;"/>
+      <h3>v0lture Solutions Policies</h3>
+      <p>v0lture Policies is currently under drafting, please check back later.</p>
+      <p>Return to main site <a href="//v0lture.com">here</a></p>
+    </div>
+
+    <div class="adblock" align="center">
+      <?php require "php/ad.php"; ?>
+    </div>
+
+  </body>
+</html>
