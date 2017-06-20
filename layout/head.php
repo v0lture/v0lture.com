d3b56e341e5519ea70f1604e9c91759bbdc75df1
Amber <maxbycode@gmail.com>
2017-06-20 12:23:59 -0600

    Dumped old site, added new dependencies and structure.

---
+++ layout/head.php
@@ -0,0 +1,13 @@
+<title>v0lture Solutions, LLC</title>
+<meta charset="utf8">
+<meta description="v0lture Solutions, LLC is a company that builds the solutions needed for your home, business, or enterprise.">
+
+<!-- resources -->
+<script src="js/jquery.js"></script>
+<script src="js/materialize.min.js"></script>
+<script src="js/app.js"></script>
+<link rel="stylesheet" href="css/materialize.min.css">
+<link rel="stylesheet" href="css/app.css">
+
+<!-- fonts -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
